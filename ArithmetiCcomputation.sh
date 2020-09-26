6333ade729b98c53ec9a26dcac2848b311bbfb32
saurabhkawade784 <saurabhkawade784@gmail.com>
2020-09-26 22:18:26 +0530

    Taking input's value from user

--- ArithmetiCcomputation.sh
+++ ArithmetiCcomputation.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 #Shell script program to calculate diffrent arithmetic operations.
 echo "Welcome to Sorting Arithmetic Computaion problem."
+#Taking input's from user.
+read -p "enter the First number :" a
+read -p "enter the Second number :" b
+read -p "enter the Third number :" c
+echo "User input values are: " $a $b $c
